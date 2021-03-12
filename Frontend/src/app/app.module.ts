5ae4eb0ddf4e726c41c05dc3739ca9c7f2315287
cinthiaCaballeroJuarez <ccinthiayessenia@yahoo.com>
2021-03-11 16:07:54 -0930

    [ADD] landingPage | comoFunciona

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ComoFuncionaComponent } from './como-funciona/como-funciona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ComoFuncionaComponent
   ],
   imports: [
     BrowserModule
