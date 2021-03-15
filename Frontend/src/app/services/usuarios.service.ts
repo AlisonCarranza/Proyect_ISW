98f95fca935707ff85df656384934b94f3982915
unknown <allisoncarranza16@gmail.com>
2021-03-14 21:11:42 -0600

    [ADD] Agregar la carpeta services que por error fue eliminada.

---
+++ Frontend/src/app/services/usuarios.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsuariosService{
+
+  constructor(private httpClient:HttpClient, private router:Router) { }
+  
+  guardarUsuario(usuario):Observable<any>{
+    return this.httpClient.post('http://localhost:4000/registro',usuario);
+  }
+
+  guardarLogin(usuarioLog):Observable<any>{
+    return this.httpClient.post('http://localhost:4000/login',usuarioLog);
+  }
+  
+  usuariosLogin(){
+  	  return !!localStorage.getItem('token');
+  }
+
+  usuariosLogout(){
+  	localStorage.removeItem('token');
+    this.router.navigate(['/menu']);
+  }
+
+}
