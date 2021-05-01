3f5ccaf7b77b73de1edf9e3649c40cb3389b2df3
unknown <allisoncarranza16@gmail.com>
2021-05-01 12:30:57 -0600

    [ADD] Visualizacion de proyectos por parte de el usuario Profesional.

--- Frontend/src/app/components/show-projects/show-projects.component.ts
+++ Frontend/src/app/components/show-projects/show-projects.component.ts
@@ -5,6 +5,8 @@ import { Router } from "@angular/router";
 import { HttpErrorResponse } from '@angular/common/http';
 import { ViewContainerRef } from '@angular/core';
 
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'abe-show-projects',
   templateUrl: './show-projects.component.html',
@@ -16,6 +18,8 @@ export class ShowProjectsComponent implements OnInit {
 
   //elementos a mostrar en la card del html
     elements: any = [];
+  //indice correspondiente del array elements
+    idElement:any;
   /*variable para la cantidad de documentos de la base y asi saber el tamaño 
     que tendra el arreglo elements*/
     docs:number;
@@ -26,9 +30,11 @@ export class ShowProjectsComponent implements OnInit {
     }
   //variable para almacenar el email del usuario logueado.
     emailUser:string='';
-  
+  //modal
+    closeResult = ''; 
   
   constructor(
+    private modalService: NgbModal,
     private cdRef: ChangeDetectorRef,
     private authService: AuthService,
     private router: Router) { }
@@ -70,16 +76,46 @@ export class ShowProjectsComponent implements OnInit {
     this.projects.forEach((project,id) => {
       console.log(project.email);
       console.log(this.emailUser);
-        this.elements.push({
-          ID:id.toString(),
-          Titulo: this.projects[id].titulo,
-          Descripcion: this.projects[id].descripcion,
-          Fecha: this.projects[id].createdAt
-        });
+      this.elements.push({
+        ID:id.toString(),
+        Titulo: this.projects[id].titulo,
+        Descripcion: this.projects[id].descripcion,
+        Fecha: this.projects[id].createdAt,
+        Roles: this.projects[id].roles,
+        Herramientas: this.projects[id].herramientas,
+        Presupuesto: this.projects[id].presupuesto,
+        Duracion:this.projects[id].timeframe,
+        CorreoUsuario:this.projects[id].email
+      });
         console.log (this.projects[id].fecha_creacion)
         console.log(this.elements[0]);
       
     });
+  }
+
+  //Funcion para traer la informacion de un proyecto en especifico.  
+    mostrarProyecto(content,id){
+      this.open(content);
+      this.idElement=parseInt(id);
+    }
+
+  //modal
+    open(content) {
+      this.modalService.open(content,{ scrollable: true }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+       }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  });
+}
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+   }
   }
 
 }
